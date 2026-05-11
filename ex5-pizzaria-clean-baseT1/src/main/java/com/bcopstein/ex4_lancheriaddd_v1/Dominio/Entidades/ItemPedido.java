188ff134e75ab53b82d83fcdd988d3dd90cb2efc
William <klein.william.contato@gmail.com>
2026-05-11 16:45:29 -0300

    infraestrutura base de pedidos

--- ex5-pizzaria-clean-baseT1/src/main/java/com/bcopstein/ex4_lancheriaddd_v1/Dominio/Entidades/ItemPedido.java
+++ ex5-pizzaria-clean-baseT1/src/main/java/com/bcopstein/ex4_lancheriaddd_v1/Dominio/Entidades/ItemPedido.java
@@ -3,12 +3,18 @@ package com.bcopstein.ex4_lancheriaddd_v1.Dominio.Entidades;
 public class ItemPedido {
     private Produto item;
     private int quantidade;
+    private double valorUnitario;
 
-    public ItemPedido(Produto item, int quantidade) {
+    public ItemPedido(Produto item, int quantidade, double valorUnitario) {
         this.item = item;
         this.quantidade = quantidade;
+        this.valorUnitario = valorUnitario;
     }
 
     public Produto getItem() { return item; }
     public int getQuantidade() { return quantidade; }
+    public double getValorUnitario() { return valorUnitario; }
+    public double getValorTotal() {
+        return quantidade * valorUnitario;
+    }
 }
